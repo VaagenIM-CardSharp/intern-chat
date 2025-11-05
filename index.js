c3eed4559f51d8b14e25964e1c90fea86baa0984
Liena Grytsyna <grytsynaliena@gmail.com>
2025-11-05 13:57:29 +0100

    Add chat message

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ io.on('connection', (socket) => {
  });
 });
 
-
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
 
 server.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
